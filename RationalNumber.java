85c04b0f1066a6f2eb19cf419f1204196880ba73
stefansor <ssorobay00@stuy.edu>
2018-10-17 11:01:49 -0400

    lab time

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,26 +1,109 @@
-public class RationalNumber extends RealNumber{
-  private double num = 42.0;
-  private int den = 1;
-  public RationalNumber(double a, int b){
-    super(a);
-    den = b;
+
+public class RationalNumber extends RealNumber
+{
+  private int numerator, denominator;
+
+  /**Initialize the RationalNumber with the provided values
+  *  if the denominator is 0, make the fraction 0/1 instead
+  *@param nume the numerator
+  *@param deno the denominator
+  */
+  public RationalNumber(int nume, int deno){
+    if (nume == 0){
+	super(nume);
+	denominator = 1;
+    }
+    if (deno == 0){
+	super(0);
+	denominator = 1; 
+    }else{
+    super(nume);
+    denominator = deno; 
+    }
   }
-  public double getNum(){
-    return num;
+
+  public double getValue(){
+    return (double)numerator / denominator;
   }
-  public int getDen(){
-    return den;
+
+  /**
+  *@return the numerator
+  */
+  public int getNumerator(){
+    return numerator;
   }
+  /**
+  *@return the denominator
+  */
+  public int getDenominator(){
+    return denominator;
+  }
+  /**
+  *@return a new RationalNumber that has the same numerator
+  *and denominator as this RationalNumber but reversed.
+  */
+  public RationalNumber reciprocal(){
+    return null;
+  }
+  /**
+  *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
+  */
+  public boolean equals(RationalNumber other){
+    return false;
+  }
+
+
+  /**
+  *@return the value expressed as "3/4" or "8/3"
+  */
   public String toString(){
-    return "" + getVal();
+    return "0";
   }
-  public boolean compare(RationalNumber x, RealNumber y){
-    return true;
+
+  /**Calculate the GCD of two integers.
+  *@param a the first integers
+  *@param b the second integer
+  *@return the value of the GCD
+  */
+  private static int gcd(int a, int b){
+    /*use euclids method or a better one*/
+    http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
+    return 0;
   }
-  public boolean compare(RealNumber x, RealNumber y){
-    return true;
+
+  /**
+  *Divide the numerator and denominator by the GCD
+  *This must be used to maintain that all RationalNumbers are
+  *reduced after construction.
+  */
+  private void reduce(){
+
   }
-  public boolean compare(RationalNumber x, RationalNumber y){
-    return true;
+  /******************Operations Return a new RationalNumber!!!!****************/
+  /**
+  *Return a new RationalNumber that is the product of this and the other
+  */
+  public RationalNumber multiply(RationalNumber other){
+    return null;
   }
-}
+
+  /**
+  *Return a new RationalNumber that is the this divided by the other
+  */
+  public RationalNumber divide(RationalNumber other){
+    return null;
+  }
+
+  /**
+  *Return a new RationalNumber that is the sum of this and the other
+  */
+  public RationalNumber add(RationalNumber other){
+    return null;
+  }
+  /**
+  *Return a new RationalNumber that this minus the other
+  */
+  public RationalNumber subtract(RationalNumber other){
+    return null;
+  }
+} 
